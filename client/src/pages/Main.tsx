4470bd3e3e8c62f209760b257914be7e0fc2773e
desuyume <77desss@gmail.com>
2023-07-21 00:04:35 +0200

    fix: remove unused useeffect for vercel

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react'
+import { FC, useState } from 'react'
 import FirstScreen from '../components/FirstScreen'
 import PagesLinks from '../components/PageLinks'
 import DonatesMarquee from '../components/DonatesMarquee'
