5d25ad543553df1adcf12e05191f6b947751049a
desuyume <77desss@gmail.com>
2023-07-23 00:42:45 +0200

    fix: scroll to top when open dangoteka page

--- client/src/pages/Dangoteka.tsx
+++ client/src/pages/Dangoteka.tsx
@@ -1,8 +1,15 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import Header from '../components/Header'
 import DangotekaSection from '../components/DangotekaSection'
+import { useLocation } from 'react-router-dom'
 
 const Dangoteka: FC = () => {
+	const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
 	return (
 		<>
 			<Header />
