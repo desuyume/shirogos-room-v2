09a97a2b2f2d30ccb7a3f2758b3451af8f7a2b90
desuyume <77desss@gmail.com>
2023-07-25 07:32:47 +0200

    feat: parse yt videos with special title in orders

--- client/src/components/OrderItem.tsx
+++ client/src/components/OrderItem.tsx
@@ -9,6 +9,26 @@ interface IOrderItem {
 	isPastOrders: boolean
 }
 
+const renderLink = ({
+	attributes,
+	content,
+}: {
+	attributes: any
+	content: string
+}) => {
+	const { href, ...props } = attributes
+
+	if (content.startsWith('https://www.youtube.com')) {
+		return (
+			<a href={href} {...props}>
+				Видео
+			</a>
+		)
+	}
+
+	return <a href={href} {...props}>{content}</a>
+}
+
 const OrderItem: FC<IOrderItem> = ({
 	index,
 	order,
@@ -17,7 +37,14 @@ const OrderItem: FC<IOrderItem> = ({
 	isPastOrders,
 }) => {
 	return (
-		<div className={(isPastOrders ? 'min-h-[2.953125rem] max-h-[2.953125rem]' : 'min-h-[3.9375rem] max-h-[3.9375rem]') + 'w-full flex justify-between items-center'}>
+		<div
+			className={
+				(isPastOrders
+					? 'min-h-[2.953125rem] max-h-[2.953125rem]'
+					: 'min-h-[3.9375rem] max-h-[3.9375rem]') +
+				'w-full flex justify-between items-center'
+			}
+		>
 			<p
 				className={
 					(isPastOrders
@@ -28,17 +55,43 @@ const OrderItem: FC<IOrderItem> = ({
 			>
 				{index}
 			</p>
-			<div className={(isPastOrders ? 'max-w-[70%]' : 'max-w-[65%]') + ' flex flex-col items-center justify-center pt-1.5 truncate'}>
-				<Linkify options={{ target: '_blank', className: 'text-[#8684EB]' }}>
-					<p className={(isPastOrders ? 'text-base text-[#A6A6A6]' : 'text-xl text-primaryText') + ' font-secondary font-bold leading-none'}>
+			<div
+				className={
+					(isPastOrders ? 'max-w-[70%]' : 'max-w-[65%]') +
+					' flex flex-col items-center justify-center pt-1.5 truncate'
+				}
+			>
+				<Linkify options={{ target: '_blank', render: renderLink }}>
+					<p
+						className={
+							(isPastOrders
+								? 'text-base text-[#A6A6A6]'
+								: 'text-xl text-primaryText') +
+							' font-secondary font-bold leading-none [&>a]:text-[#8684EB]'
+						}
+					>
 						{order}
 					</p>
 				</Linkify>
-				<p className={(isPastOrders ? 'text-[0.8125rem] text-[#9A2855]' : 'text-primary text-[0.9375rem]') + ' font-secondary font-bold -mt-1'}>
+				<p
+					className={
+						(isPastOrders
+							? 'text-[0.8125rem] text-[#9A2855]'
+							: 'text-primary text-[0.9375rem]') +
+						' font-secondary font-bold -mt-0.5'
+					}
+				>
 					{nickname}
 				</p>
 			</div>
-			<p className={(isPastOrders ? 'text-[1.0625rem] text-[#A6A6A6]' : 'text-primary text-[0.9375rem]') + ' w-[20%] text-center text-primaryText font-secondary font-bold text-xl'}>
+			<p
+				className={
+					(isPastOrders
+						? 'text-[1.0625rem] text-[#A6A6A6]'
+						: 'text-primary text-[0.9375rem]') +
+					' w-[20%] text-center text-primaryText font-secondary font-bold text-xl'
+				}
+			>
 				{time === '' ? 'Х' : time}
 			</p>
 		</div>
