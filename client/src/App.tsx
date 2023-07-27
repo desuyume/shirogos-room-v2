fc851e859e99e18fd4852179856fb7619e199da9
desuyume <77desss@gmail.com>
2023-07-28 00:30:45 +0200

    feat: add custom scroll

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRouter from './components/AppRouter'
+import SimpleBar from 'simplebar-react';
+import 'simplebar-react/dist/simplebar.min.css';
+import "./styles/scroll.css"
 
 function App() {
-  return (
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
-  )
+	return (
+		<SimpleBar className='scroll'>
+			<BrowserRouter>
+				<AppRouter />
+			</BrowserRouter>
+		</SimpleBar>
+	)
 }
 
 export default App
