199d44d1a369463b961d49bd6f8cbe3b39bf0764
desuyume <77desss@gmail.com>
2023-07-29 19:28:03 +0200

    fix: change active and hover styles on header bttns

--- client/src/components/HeaderBttn.tsx
+++ client/src/components/HeaderBttn.tsx
@@ -10,7 +10,7 @@ const HeaderBttn: FC<IHeaderBttn> = ({ path, title }) => {
 	const isActive = window.location.pathname === path
 
 	return (
-		<Link className={(isActive ? 'bg-transparent text-primary ' : 'bg-secondary text-white ') + 'rounded-[37px] text-xl w-[240px] inline-block text-center py-4 mr-5 last:mr-0'} to={path}>{title}</Link>
+		<Link className={(isActive ? 'bg-transparent text-primary cursor-default' : 'bg-secondary text-white hover:bg-secondaryHover') + ' rounded-[37px] text-xl w-[240px] inline-block text-center py-4 mr-5 last:mr-0 transition-all'} to={path}>{title}</Link>
 	);
 };
 
