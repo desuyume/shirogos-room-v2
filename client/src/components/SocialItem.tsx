3ec9921ce9437a3c179cce64e9ca09749f27bc91
desuyume <77desss@gmail.com>
2023-07-30 18:10:05 +0200

    fix: revert social hover effect

--- client/src/components/SocialItem.tsx
+++ client/src/components/SocialItem.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react'
+import { FC } from 'react'
 import { Link } from 'react-router-dom'
 
 interface ISocialItem {
@@ -8,32 +8,14 @@ interface ISocialItem {
 }
 
 const SocialItem: FC<ISocialItem> = ({ isPageLink, link, icon }) => {
-	const [isHovered, setIsHovered] = useState<boolean>(false)
-
 	return isPageLink ? (
-		<Link
-			onMouseOver={() => setIsHovered(true)}
-			onMouseLeave={() => setIsHovered(false)}
-			className='relative'
-			to={link}
-		>
-			<div className={(isHovered ? 'visible opacity-100' : 'invisible opacity-0') + ' bg-[#FFFFFF] bg-opacity-25 z-50 w-full h-full rounded-[0.9375rem] absolute transition-all'} />
+		<Link className='relative' to={link}>
+			<div className='w-full h-full rounded-[0.9375rem] absolute transition-all' />
 			<img src={icon} alt='social-con' />
 		</Link>
 	) : (
-		<a
-			onMouseOver={() => setIsHovered(true)}
-			onMouseLeave={() => setIsHovered(false)}
-			className='relative hover:[&>div]:visible'
-			href={link}
-			target='_blank'
-		>
-			<div
-				className={
-					(isHovered ? 'visible opacity-100' : 'invisible opacity-0') +
-					' bg-[#FFFFFF] bg-opacity-25 z-50 w-full h-full rounded-[0.9375rem] absolute transition-all'
-				}
-			/>
+		<a className='relative' href={link} target='_blank'>
+			<div className='w-full h-full rounded-[0.9375rem] absolute transition-all' />
 			<img src={icon} alt='social-con' />
 		</a>
 	)
