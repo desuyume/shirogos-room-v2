a401bf1b9a00eab2b10611c6ed997371690e5e46
desuyume <77desss@gmail.com>
2023-11-22 14:58:00 +0200

    chore: remove domain from cookie config

--- server/src/consts/cookieConfig.ts
+++ server/src/consts/cookieConfig.ts
@@ -1,7 +1,6 @@
 import { CookieOptions } from 'express'
 
 export const cookieConfig: CookieOptions = {
-	domain: process.env.CLIENT_DOMAIN_URL,
 	maxAge: 7 * 24 * 60 * 60 * 1000,
 	httpOnly: true,
 	sameSite: 'none',
