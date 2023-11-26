aa5fb973e57e63b0f27c47b716118aa9291219c3
desuyume <77desss@gmail.com>
2023-11-26 13:16:04 +0200

    feat(admin/awards): remove background option from award types

--- server/prisma/seeds/award-seed.ts
+++ server/prisma/seeds/award-seed.ts
@@ -3,6 +3,8 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const AwardSeed = async () => {
+  await prisma.awardType.deleteMany()
+
   await prisma.awardType.upsert({
     where: {
       id: 1,
@@ -23,52 +25,38 @@ export const AwardSeed = async () => {
     },
     create: {
       id: 2,
-      type: 'Фон',
+      type: 'Значок 1',
     },
     update: {
       id: 2,
-      type: 'Фон',
-    },
-  });
-
-  await prisma.awardType.upsert({
-    where: {
-      id: 3,
-    },
-    create: {
-      id: 3,
-      type: 'Значок 1',
-    },
-    update: {
-      id: 3,
       type: 'Значок 1',
     },
   });
 
   await prisma.awardType.upsert({
     where: {
-      id: 4,
+      id: 3,
     },
     create: {
-      id: 4,
+      id: 3,
       type: 'Значок 2',
     },
     update: {
-      id: 4,
+      id: 3,
       type: 'Значок 2',
     },
   });
 
   await prisma.awardType.upsert({
     where: {
-      id: 5,
+      id: 4,
     },
     create: {
-      id: 5,
+      id: 4,
       type: 'Значок 3',
     },
     update: {
-      id: 5,
+      id: 4,
       type: 'Значок 3',
     },
   });
