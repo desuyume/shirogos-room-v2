1b1ceff9f371f2a832a9c0ec4f53b4d7f7d0f2cd
desuyume <77desss@gmail.com>
2024-03-30 15:04:54 +0200

    style(rating): change rating bg

--- client/src/components/Rating/Rating.tsx
+++ client/src/components/Rating/Rating.tsx
@@ -12,6 +12,10 @@ const Rating: FC<IRating> = ({ className }) => {
 				'w-[26.625rem] h-[17.1875rem] bg-secondary bg-opacity-40 rounded-[2.3125rem] flex flex-col items-center pt-[0.38rem] ' +
 				className
 			}
+			style={{
+				background:
+					'linear-gradient(137deg, rgba(23, 23, 23, 0.20) 0%, rgba(36, 36, 36, 0.20) 46.88%), rgba(24, 24, 24, 0.40)',
+			}}
 		>
 			<div className='bg-secondary w-[14.6875rem] h-[2.125rem] flex justify-center items-center rounded-[2.3125rem] mb-[0.9375rem]'>
 				<p className='text-center text-primary text-[1.875rem] leading-none'>
