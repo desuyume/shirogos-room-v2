63f19190bed4c5bc60a9297e0abddf8b4318df0a
desuyume <77desss@gmail.com>
2024-07-07 18:02:01 +0200

    styles: make scale docs on hover in streamer page

--- client/src/pages/Streamer.tsx
+++ client/src/pages/Streamer.tsx
@@ -5,6 +5,7 @@ import Chronicles from '@/components/Chronicles/Chronicles'
 import watchTableImg from '@/assets/watch-table.png'
 import watchTableHoverImg from '@/assets/watch-table-hover.png'
 import Orders from '@/components/Orders/Orders'
+import { cn } from '@/utils/cn'
 
 const Streamer: FC = () => {
 	const [isDocsHover, setIsDocsHover] = useState<boolean>(false)
@@ -17,17 +18,30 @@ const Streamer: FC = () => {
 				<Orders className='mt-[31.125rem] ml-[21rem]' />
 				<Chronicles />
 				<a
-					className='w-[103px] h-[154.33px] absolute top-[37.5rem] left-[8.125rem]'
+					className='w-[103px] h-[154.33px] absolute top-[37.5rem] left-[8.125rem] hover:scale-[1.6] transition-all'
 					onMouseOver={() => setIsDocsHover(true)}
 					onMouseLeave={() => setIsDocsHover(false)}
 					href='https://docs.google.com/spreadsheets/d/1Qa0lxGo0qPGpLf2k2HsfinIy6zfg7MYlgWGRsG88Eac/edit?usp=sharing'
 					target='_blank'
 				>
-					<img
-						className='w-[103px] h-[154.33px]'
-						src={isDocsHover ? watchTableHoverImg : watchTableImg}
-						alt='docs-icon'
-					/>
+					<div className='relative'>
+						<img
+							className={cn('w-[103px] h-[154.33px] absolute transition-all', {
+								'visible opacity-100': !isDocsHover,
+								'invisible opacity-0': isDocsHover,
+							})}
+							src={watchTableImg}
+							alt='docs-icon'
+						/>
+						<img
+							className={cn('w-[103px] h-[154.33px] absolute transition-all', {
+								'visible opacity-100': isDocsHover,
+								'invisible opacity-0': !isDocsHover,
+							})}
+							src={watchTableHoverImg}
+							alt='docs-icon'
+						/>
+					</div>
 				</a>
 			</div>
 		</>
